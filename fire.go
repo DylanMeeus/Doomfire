419951e651d6dcbc00f5a5754e40f3d2e25b7b4a
dylan <meeusdylan@protonmail.com>
2019-01-08 11:15:50 +0100

    setup canvas and render functions

--- fire.go
+++ fire.go
@@ -1,9 +1,42 @@
 package main
 
 
-import "fmt"
+import (
+    "fmt"
+    "syscall/js"
+)
 
 func main(){
     fmt.Println("Hello world!")
+
+    doc := js.Global().Get("document")
+    canvas := js.Global().Get("document").Call("getElementById", "mycanvas")
+
+    bodyW := doc.Get("body").Get("clientWidth").Float()
+    bodyH := doc.Get("body").Get("clientHeight").Float()
+
+    canvas.Set("width", bodyW)
+    canvas.Set("height", bodyH)
+
+    ctx := canvas.Call("getContext", "2d")
+    _ = ctx
+
+    done := make(chan struct{}, 0) // something to keep our thread alive
+
+    var renderFrame js.Callback
+
+    renderFrame = js.NewCallback(func(args []js.Value) {
+        fmt.Println("output!")
+        js.Global().Call("requestAnimationFrame", renderFrame)
+    })
+
+    defer renderFrame.Release()
+
+    // start
+    js.Global().Call("requestAnimationFrame", renderFrame)
+
+    <-done
+    
+
 }
 
